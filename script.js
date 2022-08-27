9f9c549986a5dfa4b711ef40e95e071e135d9703
Rayandson <rayandson.silva321@gmail.com>
2022-08-27 01:10:33 -0300

    sidebar

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 let username = "";
-
-username = prompt("Digite seu nome");
-let nome = {name: `${username}`};
-verificaNome();
+let nome;
 function verificaNome() {
+    let aguarde = document.querySelector(".carregando");
+    aguarde.classList.remove("escondido");
+    username = document.querySelector(".username").value;
+    nome = {name: `${username}`};
 const requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nome);
-requisicao.then(function (){console.log("Nome enviado")});
+requisicao.then(function(){setTimeout(iniciaAplicacao, 500)});
 requisicao.catch(processaErro);
 }
 function processaErro(erro){
     if(erro.response.status !== 200){
-        username = prompt("O nome já existe! Escolha outro.");
-        verificaNome();
+        alert("O nome já existe! Escolha outro.");
+        let div = document.querySelector(".carregando");
+        div.classList.add("escondido");
     }  
+        
+
+}
+function iniciaAplicacao() {
+    let text = document.querySelector(".entrando-texto");
+    text.innerHTML = "Entrando...";
+    let div = document.querySelector(".login");
+    div.classList.add("escondido");
+    const breakbusca = setInterval(buscarMensagens, 3000);
+    const buscaParticipants = setInterval(buscarParticipantes, 3000);
+    const breakstatus = setInterval(verificaStatus, 5000);
 
 }
 
-const breakstatus = setInterval(verificaStatus, 5000);
-let i = 0;
+
 function verificaStatus() {
+    nome = {name: `${username}`};
     let requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/status",nome);
     requisicao.then(function (){console.log("Tô online")});
     requisicao.catch(function (){console.log("Deu erro")});
-    i++;
 }
-const breakbusca = setInterval(buscarMensagens, 3000);
-const buscaParticipants = setInterval(buscarParticipantes, 3000);
+// const breakbusca = setInterval(buscarMensagens, 3000);
+// const buscaParticipants = setInterval(buscarParticipantes, 3000);
 // buscarMensagensEparticipantes();
 function buscarMensagens(){
     let requisicao = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
@@ -34,6 +46,8 @@ function buscarMensagens(){
 }
 
 function renderizarMensagens(resposta){
+    let aguarde = document.querySelector(".carregando");
+    aguarde.classList.add("escondido");
     let mensagens = document.querySelector(".content");
     mensagens.innerHTML = "";
     // console.log(resposta);
@@ -41,13 +55,11 @@ function renderizarMensagens(resposta){
         if(resposta.data[i].type === "message") {
         mensagens.innerHTML = mensagens.innerHTML + `<div class="div-msg publica">
         <div class="hora"><p>${resposta.data[i].time}</p></div>
-        <div class="descricao"><p><span>${resposta.data[i].from}</span> para <span>${resposta.data[i].to}</span>:</p></div>
-        <div class="msg">${resposta.data[i].text}</div>`
+        <div class="descricao"><p><span class="user">${resposta.data[i].from}</span> para <span class="user">${resposta.data[i].to}</span>: <span class="msg">${resposta.data[i].text}</span></p></div>`
         }else if(resposta.data[i].type === "status") {
             mensagens.innerHTML = mensagens.innerHTML + `<div class="div-msg status">
             <div class="hora"><p>${resposta.data[i].time}</p></div>
-            <div class="descricao"><p><span>${resposta.data[i].from}</span></p></div>
-            <div class="msg">${resposta.data[i].text}</div>` 
+            <div class="descricao"><p><span class="user">${resposta.data[i].from}</span> <span class="msg">${resposta.data[i].text}</span></p></div>` 
         }
     }
     const ultimaMensagem = document.querySelector('.content').lastElementChild;
@@ -81,4 +93,10 @@ function enviarMensagem(){
 
 function Recarregar() {
     window.location.reload();
+}
+
+function mostraSidebar() {
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.classList.remove("escondido");
+    console.log(sidebar.classList);
 }
